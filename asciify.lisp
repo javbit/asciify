02cd77e5135af5471d91e806a590d8b5da0af1d4
Javed Mohamed <javbit@disroot.org>
2020-07-24 11:27:28 -0500

    Iterate through image pixels

--- asciify.lisp
+++ asciify.lisp
@@ -6,4 +6,10 @@
       (format t "Successfully loaded ~s!~%" filename)
       (format t "Image size: ~sx~s~%"
 	      (imago:image-width image)
-	      (imago:image-height image)))))
+	      (imago:image-height image))
+      (imago:do-image-pixels
+       (image color x y)
+       (format t "~s ~s ~s~%"
+	       (imago:color-red color)
+	       (imago:color-green color)
+	       (imago:color-blue color))))))
