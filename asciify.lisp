e0a9f6235054f58593c2e3e4e98185370abe8d10
Javed Mohamed <javbit@disroot.org>
2020-07-24 22:09:55 -0500

    Untabify source

--- asciify.lisp
+++ asciify.lisp
@@ -15,32 +15,32 @@
 
 (defmacro with-rgb (color r g b &body body)
   `(let ((,r (imago:color-red ,color))
-	 (,g (imago:color-green ,color))
-	 (,b (imago:color-blue ,color)))
+         (,g (imago:color-green ,color))
+         (,b (imago:color-blue ,color)))
      ,@body))
 
 (defun brightness (color)
   (with-rgb color red green blue
-	    (floor (+ red green blue) 3)))
+            (floor (+ red green blue) 3)))
 
 (defun lightness (color)
   (with-rgb color red green blue
-	    (floor (+ (max red green blue)
-		      (min red green blue))
-		   2)))
+            (floor (+ (max red green blue)
+                      (min red green blue))
+                   2)))
 
 (defun luminance (color)
   (with-rgb color red green blue
-	    (floor (+ (* Pr red)
-		      (* Pg green)
-		      (* Pb blue)))))
+            (floor (+ (* Pr red)
+                      (* Pg green)
+                      (* Pb blue)))))
 
 (defun pixel-brightness (color method)
   (case method
-	(:brightness (brightness color))
-	(:lightness (lightness color))
-	(:luminance (luminance color))
-	(otherwise (imago:color-intensity color))))
+        (:brightness (brightness color))
+        (:lightness (lightness color))
+        (:luminance (luminance color))
+        (otherwise (imago:color-intensity color))))
 
 (defun pixel->ascii (pixel method)
   "Convert PIXEL to ASCII character using CONVERSION method."
@@ -53,9 +53,9 @@
 
 (defun colorize (color)
   (let ((red (write-to-string (imago:color-red color)))
-	(green (write-to-string (imago:color-green color)))
-	(blue (write-to-string (imago:color-blue color)))
-	(bg (write-to-string *bg-color*)))
+        (green (write-to-string (imago:color-green color)))
+        (blue (write-to-string (imago:color-blue color)))
+        (bg (write-to-string *bg-color*)))
     (concatenate 'string ESCAPE "[" bg ":2:" red ":" green ":" blue "m")))
 
 (defconstant CLEAR
@@ -64,9 +64,9 @@
 (defun print-pixel (pixel method colorp)
   (let ((shape (string (pixel->ascii pixel method))))
     (write (if colorp
-	       (concatenate 'string (colorize pixel) shape CLEAR)
-	     shape)
-	   :escape nil)))
+               (concatenate 'string (colorize pixel) shape CLEAR)
+             shape)
+           :escape nil)))
 
 (defun newline ()
   (write (string #\Newline) :escape nil))
@@ -75,7 +75,7 @@
   (let ((image (imago:resize (imago:read-image filename) 80 23)))
     (when image
       (imago:do-image-pixels (image pixel x y)
-			     (when (and (zerop x) (not (zerop y)))
-			       (newline))
-			     (print-pixel pixel method colorp))
+                             (when (and (zerop x) (not (zerop y)))
+                               (newline))
+                             (print-pixel pixel method colorp))
       (newline))))
